38d9b9aa81929290f771675bacedd55f01057b90
Samu Kupiainen <kupiainen@testausserveri.fi>
2024-06-21 16:41:13 +0300

    fix: remove unused import

--- styles/themes.ts
+++ styles/themes.ts
@@ -2,7 +2,7 @@
  * Themes
  */
 import Colors from './colors';
-import { adaptNavigationTheme, configureFonts } from 'react-native-paper';
+import { configureFonts } from 'react-native-paper';
 
 const fonts = configureFonts({ config: { fontFamily: 'Gaba' } });
 
