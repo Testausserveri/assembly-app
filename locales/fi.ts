b89e24fbe6c98037ef3b3c1bf2e633a4c76f87fd
Samu Kupiainen <kupiainen@testausserveri.fi>
2024-06-22 16:37:52 +0300

    feat: create fi locale

---
+++ locales/fi.ts
@@ -0,0 +1,9 @@
+export default {
+    translation: {
+        home: 'Koti',
+        useless: 'Useless',
+        timetable: 'Aikataulu',
+        location: 'Sijainti',
+        time: 'Aika',
+    },
+};
