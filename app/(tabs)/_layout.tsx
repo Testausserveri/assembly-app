912ad7843385754ed56a70bda8cf796477a45dda
Samu Kupiainen <kupiainen@testausserveri.fi>
2024-07-15 20:56:55 +0300

    fix: files created during rebase

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,7 +37,7 @@ export default function TabLayout() {
                 }}
             />
             <Tabs.Screen
-                name='info'
+                name='about'
                 options={{
                     tabBarIcon: ({ color, focused }) => (
                         <TabBarIcon
