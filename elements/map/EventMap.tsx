e2a6b1458a5aaf08b678d1fd8898adcbc5062427
Samu Kupiainen <kupiainen@testausserveri.fi>
2024-07-28 12:54:10 +0300

    fix: update map

--- elements/map/EventMap.tsx
+++ elements/map/EventMap.tsx
@@ -4,23 +4,20 @@ import { Image, View } from 'react-native';
 function EventMap() {
     return (
         <ReactNativeZoomableView
-            maxZoom={2.5}
-            minZoom={0.5}
+            maxZoom={0.3}
+            minZoom={0.05}
             zoomStep={0.5}
-            initialZoom={0.5}
+            initialZoom={0.05}
             bindToBorders={true}
             style={{
                 width: 'fit-content',
             }}
             // Image dimensions
-            contentWidth={1024}
-            contentHeight={576}
+            contentWidth={10000}
+            contentHeight={5625}
         >
             <View style={{ width: '100%' }}>
-                <Image
-                    style={{ objectFit: 'contain' }}
-                    source={require('@/assets/images/summer23-aluekartta.png')}
-                />
+                <Image source={require('@/assets/images/summer24-aluekartta.png')} />
             </View>
         </ReactNativeZoomableView>
     );
